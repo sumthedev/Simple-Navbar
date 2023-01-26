e4bfb4c4bcbbf8b581c4f084d7584a5a2554becc
Sumbal Hashmi <hashmisumbal12@gmail.com>
2023-01-26 15:19:12 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Simple function
+
+function myFunction() {
+  var navBar = document.getElementById("myTopnav");
+  if (navBar.className === "topnav") {
+    navBar.className += " responsive";
+  } else {
+    navBar.className = "topnav";
+  }
+}
